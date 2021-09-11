79ec5ebf884ec89663c36c2183dc2ee13145579f
AMPalatov <eorit-wanderer@yandex.ru>
2021-09-11 18:27:06 +0300

    lesson9

---
+++ 9.cpp
@@ -0,0 +1,188 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#define SZ 10
+#define T int
+#define SIZE 1000
+#define true 1==1
+#define false 1!=1
+typedef struct {
+    int pr;
+    int dat;
+} Node;
+
+Node* arr[SZ];
+int head;
+int tail;
+int items;
+
+void init() {
+    for (int i = 0; i < SZ; ++i) {
+        arr[i] = NULL;
+    }
+    head = 0;
+    tail = 0;
+    items = 0;
+}
+
+
+void ins(int pr, int dat) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->dat = dat;
+    node->pr = pr;
+    int flag;
+    if (items == SZ) {
+        printf("%s \n", "Queue is full");
+        return;
+    }
+    else {
+        arr[tail++] = node;
+        items++;
+    }
+}
+
+Node* rem() {
+    int flag;
+    if (items == 0) {
+        return NULL;
+    }
+    else {
+        int i = 0;
+        int idx = 0;
+        Node* tmp;
+        int temp_i=0;
+        for (i = head; i < tail-1; ++i) {
+            idx = i % SZ;
+            if (arr[(i+1)%SZ]->pr > arr[idx]->pr) {
+                tmp = arr[(i + 1) % SZ];
+                temp_i = i+1;
+                idx++;
+            }
+        }
+        
+        i = temp_i;
+        printf("Out [%d, %d]\n", arr[i]->pr, arr[i]->dat);
+        flag = i % SZ;
+        while (i < tail) {
+            if (i == tail-1) {
+                idx = i % SZ; 
+                arr[idx] = NULL;
+                i++;
+            }
+            else {
+                idx = i % SZ;
+                arr[idx] = arr[(i + 1) % SZ];
+                i++;
+            }
+        }
+        items--;
+        tail--;
+    }
+}
+    
+    /*else {
+        int idx = head++ % SZ;
+        Node* tmp = arr[idx];
+        arr[idx] = NULL;
+        items--;
+        return tmp;
+    }
+}*/
+
+
+void printQueue() {
+    printf("Actual Queue [ ");
+    for (int i = 0; i < SZ; ++i) {
+        if (arr[i] == NULL)
+            printf("[*,*]");
+        else
+            printf("[%d, %d]", arr[i]->pr, arr[i]->dat);
+
+    }
+    printf("]\n\n");
+}
+/*
+9
+1 описать очередь с приоритетным исключением
+2 реализоваьт перевод из десфятичной в двочиную систему счисления с использованием стека
+10
+1 написать программу, которая определяет является ли веденая скгоьбочная
+последвоательность правильной. Примеры правильных скобочных последовательностей
+()..
+2 Создать функцию, копирующую односвязный списко(без удаления первого списка)
+3 реализуйте алгритм. которые определяет, отсортирован ли связный список
+*/
+typedef int boolean;
+int cursor = -1;
+T stack[SIZE];
+
+boolean push(T data) {
+    if (cursor < SIZE) {
+        stack[++cursor] = data;
+        return true;
+    }
+    else {
+        printf("%s \n", "Stack overflow");
+    }
+}
+
+T pop() {
+    if (cursor != -1) {
+        return stack[cursor--];
+    }
+    else {
+        printf("%s \n", "Stack is empty");
+        return -1;
+    }
+}
+
+
+int main() {
+//Task9.1==========================
+
+    int num;
+    scanf("%d", &num);
+    while (num > 0) {
+        if (num == 1){
+            push(1);
+            num = 0;
+        }
+        else {
+            push(num % 2);
+            num = num / 2;
+        }
+    }
+    while (cursor != -1) {
+        printf("%d", pop());
+    }
+
+    //Task9.2==========================
+
+    init();
+    ins(1, 11);
+    ins(3, 22);
+    ins(4, 33);
+    ins(5, 44);
+    ins(4, 55);
+    ins(6, 66);
+    ins(7, 77);
+    ins(9, 88);
+    ins(8, 99);
+    ins(6, 1);
+    printQueue();
+
+    for (int i = 0; i < SZ/2; i++) {
+        rem();
+        printQueue();
+    }
+    
+    ins(8, 77);
+    ins(8, 88);
+    ins(7, 99);
+    for (int i = 0; i < SZ; i++) {
+        rem();
+        printQueue();
+    }
+  
+    
+    return 0;
+}
---
+++ L4.cpp
@@ -0,0 +1,183 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+#define HEIGHT 8
+#define WIDTH 8
+#define KING HEIGHT * WIDTH - 1
+
+
+
+void PrintArray(int* array, int len) {
+    for (int i = len-1; i >= 0; i--) {
+        printf("%d", array[i]);
+    }
+}
+
+//int* InitArray(int A) {
+//    int* array = new int[A/2]
+//}
+
+
+
+void Task1(int A) {
+    //int B[10] = { 0 };
+    /*for (int i = 0; i < 10; i++) {
+        B[i] = 0;
+    }*/
+    //static int step = -1;
+    //DecToBi(A, B, step);
+    //char rslt[] = {0};
+    binr(A);
+    //PrintArray(B, 10);
+    //printf("%c", rslt);
+}
+
+void binr(int n) {
+    if (n <= 0) return;
+    binr(n / 2);
+    printf("%d", ((n % 2 == 0) ? "0" : "1"));
+}
+
+int DecToBi (int a, int* array, int step){
+    //char C[100] = {1};
+    //int B[1000] = {0};
+    //int i = 0;
+/*	while (A>=1){
+		B=(A)%2;
+
+		printf("%d", A);
+		printf("%d", B);
+		A=A/2;
+		sprintf(C, "%d", B);
+   	}
+*/
+    if (a>=1) {
+        step = step + 1;
+        array[step] = ((DecToBi(a / 2,array ,step)) % 2);
+        //printf("%d", a / 2);
+        return (a % 2);
+    }
+    
+}
+//=============================================================
+/*Возвежение числа а в степень в рекурсивно*/
+int rise_rec(int A, int B) {
+    if (B > 0) {
+        A = A * rise_rec(A, B - 1);
+        return A;
+    }
+}
+//============================================================
+/*Рекурсивно, используя свойство чётности степени (то есть, если степень, в которую нужно возвести число, чётная, 
+основание возводится в квадрат, а показатель делится на два, а если степень нечётная - результат 
+умножается на основание, а показатель уменьшается на единицу)*/
+//==============================================================
+int rise_rec2(int A, int B, int C) {
+    int res;
+    if (B >1 && B % 2 == 0) {
+        A = rise_rec2(A *A, B / 2, C);
+        //C = A;
+        //B = B / 2;
+    }
+    else if (B >= 1 && B % 2 != 0) {
+        A = A*rise_rec2(A, B - 1, C);
+        //B = B - 1;
+    }
+    else if (B == 1) return A;
+    else return 1;
+    //printf("%d", A);
+    return A;
+ 
+}
+
+/*===============================================================================
+Реализовать нахождение количества маршрутов шахматного короля с препятствиями 
+(где единица - это наличие препятствия, а ноль - свободная для хода клетка).
+Король ходит только по прямой, маршруты считаются с клетки (0, 0)*/
+int routes(int x, int y) {
+    if (x == 0 && y == 0)
+        return 0;
+    else if (x == 0 ^ y == 0)
+        return 1;
+    else
+        return routes(x, y - 1) + routes(x - 1, y);
+}
+//=========================================
+int board[HEIGHT][WIDTH];
+void printBoard() {
+    for (int i = 0; i < HEIGHT; ++i)
+        for (int j = 0; j < WIDTH; ++j)
+            printf("3%d", board[i][j]);
+    printf("\n");
+     }
+int possible[][2] = {
+    {-1, 1}, {-1,0}, {-1,-1},
+    {0, -1}, {0, 1}, {-1, 0}, {1, 0},
+    {1, 1}, {1,0}, {1, -1} };
+
+
+int isPossible(int x, int y) {
+    return x > 0 && x < WIDTH&& y>0 && y < HEIGHT
+        && board[x][y] == 0;
+}
+
+int kingMove(int x, int y, int move) {
+    int nextX;
+    int nextY;
+    board[y][x] = move;
+    if (move > KING) return 1;
+    for (int i = 0; i < 10; ++i) {
+        nextX = x + possible[i][1];
+        nextY = y + possible[i][0]; 
+        if (isPossible(nextX, nextY) && kingMove(nextX, nextY, move + 1))
+            return 1;
+        }
+        board[y][x] = 0;
+        return 0;
+}
+        
+int main() {
+    //========Задача 1
+    int A;
+    scanf("%d", &A);
+    Task1(A);
+    
+    //===========Задача 2.1
+    int N;
+    int E;
+    int res;
+    scanf("%d", &N);
+    scanf("%d", &E);
+    res = rise_rec(N, E);
+    printf("%d", res);
+
+    // ========== задча2.2
+    int Z;
+     int X;
+     //int res;
+     scanf("%d", &Z);
+     scanf("%d", &X);
+     printf ("%d", rise_rec2(Z, X, Z));
+     //printf("%d", res);
+     
+     //===========задача3
+    
+   // const int f = 1;  //какой-то странный глюк в MS visual - первое значение не определяется средойт разраюотки как объявленное
+    //const int sizeY = 5;
+    //const int sizeX = 5;
+    //for (int y = 0; y < sizeY; y++) {
+    //    for (int x = 0; x < sizeX;  ++x) {
+     //       printf("%5d", routes(x, y));
+      //  }
+     //   printf("\n");
+   // }
+   // return 0;
+
+    kingMove(0, 0, 1);
+    printBoard();
+    
+    char l;                     //что бы консоль не закрывалась сразу
+    scanf("%s", &l);
+
+}
